ae894c8a2b3854506596307d1d9f523b52c7091c
Abdullah Omar <fishilike.fish@gmail.com>
2019-11-11 02:53:22 +0600

    Setup add for Heroku

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -21,7 +21,7 @@ weatherForm.addEventListener('submit', e => {
   e.preventDefault();
   // console.log(location);
 
-  fetch('http://localhost:3000/weather?address=' + locationName).then(res => {
+  fetch('/weather?address=' + locationName).then(res => {
     res.json().then(data => {
       if (data.error) {
         // console.log(data.error);
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 const hbs = require('hbs');
 const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
@@ -120,6 +121,6 @@ app.get('*', (req, res) => {
 
 //================================================================================================================
 
-app.listen(3000, () => {
-  console.log('Server is running in port 3000');
+app.listen(port, () => {
+  console.log('Server is running in port ' + port);
 });

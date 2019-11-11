a7def426b1009973f976f69f41ff1375d3fdb8b3
Abdullah Omar <fishilike.fish@gmail.com>
2019-11-12 00:47:50 +0600

    Display more weather features

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitide, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location', undefined);
     } else {
-      callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain');
+      callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.' + ' Current summary: ' + body.hourly.summary);
     }
   });
 };
